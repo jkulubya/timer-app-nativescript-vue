88bd9cccf9a7db76a5e3e280a19e8d9d8ad78936
jkulubya <jameskitamirike@gmail.com>
2017-11-18 11:22:49 +0200

    Nav

--- app/components/App.vue
+++ app/components/App.vue
@@ -1,16 +1,33 @@
 <template>
-    <page>
-        <label></label>
-        <button></button>
-        <button></button>
+    <page actionBarHidden="true">
+        <stack-layout>
+            <grid-layout columns="*,*,*" rows="*,2*,*" height="240">
+                <label text="+" col="0" row="0"></label>
+                <label text="+" col="1" row="0"></label>
+                <label text="+" col="2" row="0"></label>
+                <label text="2hrs" col="0" row="1"></label>
+                <label text="10min" col="1" row="1"></label>
+                <label text="7s" col="2" row="1"></label>
+                <label text="-" col="0" row="2"></label>
+                <label text="-" col="1" row="2"></label>
+                <label text="-" col="2" row="2"></label>
+            </grid-layout>
+            <button text="Start" @tap="onTap"></button>
+        </stack-layout>
     </page>
 </template>
 
 <script>
+    import ViewTimer from "./ViewTimer.vue";
     export default {
         data() {
             return {
                 count: 10
+            }
+        },
+        methods: {
+            onTap() {
+                this.$navigateTo(ViewTimer)
             }
         }
     }
---
+++ app/components/ViewTimer.vue
@@ -0,0 +1,24 @@
+<template>
+    <page actionBarHidden="true">
+        <stack-layout>
+            <grid-layout columns="*" rows="3*,*,*" height="240">
+                <label text="02:16:47" col="0" row="0"></label>
+                <button text="||" col="0" row="1"></button>
+            </grid-layout>
+            <button text="Start"></button>
+        </stack-layout>
+    </page>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                count: 10
+            }
+        }
+    }
+</script>
+
+<style scoped>
+</style>
