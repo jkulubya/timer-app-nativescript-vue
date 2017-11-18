4b544c97efb11806b3af2ce9e02abade8ec541a5
jkulubya <jameskitamirike@gmail.com>
2017-11-18 10:37:35 +0200

    Initial commit

---
+++ app/components/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <page>
+        <label></label>
+        <button></button>
+        <button></button>
+    </page>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                count: 10
+            }
+        }
+    }
+</script>
+
+<style scoped>
+</style>
